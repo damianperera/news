e7462afa6bca81543e429750ee2df2b2d89f82b0
Damian Perera <damian.perera@personio.de>
2021-09-09 17:05:49 +0200

    updated main file

--- scraper/main.go
+++ scraper/main.go
@@ -1,1 +1,26 @@
-package scraper
+package main
+
+import "fmt"
+import "github.com/gocolly/colly/v2"
+
+func main() {
+	fmt.Println("Hello, World!")
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		err := e.Request.Visit(e.Attr("href"))
+		if err != nil {
+			return
+		}
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	err := c.Visit("http://go-colly.org/")
+	if err != nil {
+		return
+	}
+}
